dec42773c412f8c8bd325c4e36d25b77faba6941
Jonathan Rockway <jon@jrock.us>
2010-11-08 07:49:51 -0600

    comment out bloat for release.

--- lib/nonsense.pm
+++ lib/nonsense.pm
@@ -11,40 +11,40 @@ sub unimport {
     strict->import;
     warnings->import;
     true->import;
-    return $into unless defined $command;
+    # return $into unless defined $command;
 
-    if( $command eq 'class' ){
-        require Moose;
-        Moose->import({ into => $into });
-    }
-    elsif( $command eq 'role' ){
-        require Moose::Role;
-        Moose::Role->import({ into => $into });
-    }
-    elsif( $command eq 'library' ){
-        require Sub::Exporter;
-        Sub::Exporter::setup_exporter({
-            into => $into,
-            @args,
-        });
-    }
-    elsif( $command eq 'type library' ){
-        require MooseX::Types;
-        require MooseX::Types::Moose;
-        MooseX::Types::Moose->import({ into => $into }, ':all');
+    # if( $command eq 'class' ){
+    #     require Moose;
+    #     Moose->import({ into => $into });
+    # }
+    # elsif( $command eq 'role' ){
+    #     require Moose::Role;
+    #     Moose::Role->import({ into => $into });
+    # }
+    # elsif( $command eq 'library' ){
+    #     require Sub::Exporter;
+    #     Sub::Exporter::setup_exporter({
+    #         into => $into,
+    #         @args,
+    #     });
+    # }
+    # elsif( $command eq 'type library' ){
+    #     require MooseX::Types;
+    #     require MooseX::Types::Moose;
+    #     MooseX::Types::Moose->import({ into => $into }, ':all');
 
-        my $types = join ', ', map { '"'. quotemeta($_). '"' } @args;
-        eval "package $into; MooseX::Types->import(-declare => [$types])";
-    }
-    else {
-        die "unknown command $command";
-    }
+    #     my $types = join ', ', map { '"'. quotemeta($_). '"' } @args;
+    #     eval "package $into; MooseX::Types->import(-declare => [$types])";
+    # }
+    # else {
+    #     die "unknown command $command";
+    # }
 
-    if( $command eq 'class' || $command eq 'role' ){
-        namespace::autoclean->import(
-            -cleanee => $into,
-        );
-    }
+    # if( $command eq 'class' || $command eq 'role' ){
+    #     namespace::autoclean->import(
+    #         -cleanee => $into,
+    #     );
+    # }
 
     return $into;
 }
